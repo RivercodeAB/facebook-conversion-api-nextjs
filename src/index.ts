478d0de2bc832a6220c41b9f521f2ad73dcd87cb
Matt Reid <6438781+mattreid1@users.noreply.github.com>
2023-12-30 17:11:35 +0000

    Exports server-side event and request utils

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { fbPageView, fbEvent } from './conversion-api';
+import { sendServerSideEvent } from './services/server-side-events';
+import { getClientIpAddress, getClientFbp, getClientFbc } from './utils/request';
 
 export {
   fbPageView,
   fbEvent,
+	getClientIpAddress,
+	getClientFbp,
+	getClientFbc,
+	sendServerSideEvent
 };
